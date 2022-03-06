cc7f627f9b7707820c939b27f73ce3fd93072af2
Yanis Kampe <yanis.kampe.cv@gmail.com>
2022-03-06 15:04:27 +0000

    Refactor

--- src/main/java/FilterCars.java
+++ src/main/java/FilterCars.java
@@ -1,12 +1,15 @@
 import Models.Car;
+import Models.CarFilter;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static Models.CarFilter.makeFilter;
+
 public class FilterCars {
 
     public static List<Car> filterCars(List<Car> carList, List<String> make) {
-        List<Car> filteredList = carList.stream().filter(car -> make == null || make.contains(car.getMake())).collect(Collectors.toList());
+        List<Car> filteredList = carList.stream().filter(makeFilter(make)).collect(Collectors.toList());
         return filteredList;
     }
 }
--- src/main/java/Models/CarFilter.java
+++ src/main/java/Models/CarFilter.java
@@ -1,10 +1,13 @@
 package Models;
 
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CarFilter {
 
-    public static Predicate<Car> makePredicate(String make) {
-        return car -> car.getMake().equals(make);
+    public static Predicate<Car> makeFilter(List<String> make) {
+        return car -> make == null || make.contains(car.getMake());
     }
+
 }
